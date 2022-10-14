7281ba8ba9cec05ee54057175dfc880f00529cf5
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-10-14 15:37:55 -0400

    fix get user route to not send back password and updatedat from user

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ const bcrypt = require("bcrypt");
 router.get("/:id", async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    // so we dont send unneeded info to client
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
   } catch (err) {
     res.status(500).json(err.message);
   }
