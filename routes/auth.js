714092affb5d17ce152c123cf6c6891cb27a056b
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-10-13 21:09:24 -0400

    fix typo

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.post("/register", async (req, res, next) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.username,
+    password: req.body.password,
   });
 
   try {
