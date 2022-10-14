9c31f7c8cd1b65a14d1c2d5977e9cbc2ddc82530
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-10-14 17:55:42 -0400

    add delete route for single post

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,10 @@ const Post = require("../models/Post");
 
 // create a post
 router.post("/", async (req, res, next) => {
+  // check to see if body has userId since its required
   if (req.body.userId) {
     try {
+      // create new post with all the data from body
       const post = new Post(req.body);
       const savedPost = await post.save();
       res.status(200).json(savedPost);
@@ -16,7 +18,23 @@ router.post("/", async (req, res, next) => {
   }
 });
 // update a post
+
 // delete a post
+router.delete("/:id", async (req, res, next) => {
+  try {
+    // find post and check whether userId matches the user whos trying to delete
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await Post.findByIdAndDelete(req.params.id);
+      res.status(200).json("post deleted");
+    } else {
+      res.status(403).json("you can only delete your own posts");
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 // like a post
 // get a post
 // get timeline posts (all posts of user's followings)
