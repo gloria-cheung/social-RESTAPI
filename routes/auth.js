20b47ddabfe1401a272450b9be0b1edb1260475d
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-10-13 21:33:47 -0400

    import bcrypt and use to hash password before saving to db and sending user back to client

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // register
 router.post("/register", async (req, res, next) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // generate new password with bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //generate new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
