4d38810515565aa0610bd97f65696e2f33f2fcf0
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-10-18 20:15:33 -0400

    change get route for single user to use query instead of params

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,15 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //get a user
-router.get("/:id", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+
   try {
-    const user = await User.findById(req.params.id);
+    // check which query is in url and search db accordingly
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
     // so we dont send unneeded info to client
     const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
